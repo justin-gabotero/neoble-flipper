abccbe9658ede8941434c1973afad2f26f612843
shin-envy <shinjozyro@gmail.com>
2023-02-25 22:25:12 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+mod request;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    request::get_uuid_from_username("3nbi");
+    Ok(())
 }
---
+++ src/request.rs
@@ -0,0 +1,16 @@
+use std::error::Error;
+use serde_json::{Result, Value};
+#[tokio::main]
+pub(crate) async fn get_uuid_from_username(name: &str) -> Result<()> {
+    let url:String = "https://api.mojang.com/users/profiles/minecraft/".to_owned() + name;
+    let res = reqwest::get(url)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+    let parsed: Value = serde_json::from_str(&res).unwrap();
+    println!("Username: {}, Id: {}", parsed["name"].as_str().unwrap(), parsed["id"].as_str().unwrap());
+    Ok(())
+}
+
