23a461df59d7fd1bfdc35630d494d7507f717645
shin-envy <shinjozyro@gmail.com>
2023-02-25 22:35:22 +0800

    fix

--- src/request.rs
+++ src/request.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use serde_json::{Result, Value};
 #[tokio::main]
-pub(crate) async fn get_uuid_from_username(name: &str) -> Result<()> {
+pub(crate) async fn get_uuid_from_username(name: &str) {
     let url:String = "https://api.mojang.com/users/profiles/minecraft/".to_owned() + name;
     let res = reqwest::get(url)
         .await
@@ -11,6 +11,5 @@ pub(crate) async fn get_uuid_from_username(name: &str) -> Result<()> {
         .unwrap();
     let parsed: Value = serde_json::from_str(&res).unwrap();
     println!("Username: {}, Id: {}", parsed["name"].as_str().unwrap(), parsed["id"].as_str().unwrap());
-    Ok(())
 }
 
